6f9b35bd278e49e38831549e2d15da71579ce05f
Rysinal <133035515@qq.com>
2018-05-23 13:29:50 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let http = require("http");
+let https = require("https");
+let fs = require("fs"); // Configuare https
+const httpsOption = {
+	key: fs.readFileSync("./https/xxxxxxxxxxxx.key"),
+	cert: fs.readFileSync("./https/xxxxxxxxxxxx.pem")
+}
+// Create service
+let app = express();
+http.createServer(app).listen(80);
+https.createServer(httpsOption, app).listen(443);
